99b7d8150eae7f288d02cc95ebf47c81cd101655
Niall <nialldbarber@gmail.com>
2019-10-02 17:30:41 +0100

    Initial commit

---
+++ src/app.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from 'react'
+
+const App: FC = () => (
+	<div>
+		<h1>Im da App</h1>
+		<p>yes i am</p>
+	</div>
+)
+
+export default App
---
+++ src/pages/home/index.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from 'react'
+
+const Home: FC = () => (
+	<div>
+		<h1>Im da App</h1>
+		<p>yes i am</p>
+	</div>
+)
+
+export default Home
