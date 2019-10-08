a6d03230955d07cd15fc1bf57272277b0a7e2137
Niall <nialldbarber@gmail.com>
2019-10-08 10:00:32 +0100

    Added styled components variables

---
+++ src/styles/styled-components/variables.js
@@ -0,0 +1,12 @@
+export const theme = {
+  mainFont: "'Roboto Mono', monospace",
+  white: '#fff',
+  offWhite: '#ededed',
+  purple: '#777bf8',
+  lightBlue: '#68d4f8',
+  brightGreen: '#3ecf8e',
+  bs: '0 2px 2px -1px rgba(0,0,0,.35)',
+  effect: 'cubic-bezier(0.21, 0.67, 0.36, 0.94)',
+  time: '0.35s',
+  ragError: '#E53E6C',
+}
