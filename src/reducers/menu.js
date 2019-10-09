8c152f10ccf3574a88e36d7be95c00383c433ead
Niall <nialldbarber@gmail.com>
2019-10-09 12:25:31 +0100

    Destructured hamburger reducer for clarity

--- src/reducers/menu.js
+++ src/reducers/menu.js
@@ -1,11 +1,17 @@
 import { CHANGE_HAMBURGER, CLOSE_HAMBURGER } from '~@constants/hamburger'
 
-export const menuReducer = (state, action) => {
-	switch (action.type) {
+export const menuReducer = (state, { type }) => {
+	switch (type) {
 		case CHANGE_HAMBURGER:
-			return { ...state, navOpen: !state.navOpen }
+			return {
+				...state,
+				navOpen: !state.navOpen
+			}
 		case CLOSE_HAMBURGER:
-			return { ...state, navOpen: false }
+			return {
+				...state,
+				navOpen: false
+			}
 		default:
 			return state
 	}
