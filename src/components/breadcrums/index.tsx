b7bc4c6f72fd94798ba5173a2ce9fb79cfb8d17a
Niall <nialldbarber@gmail.com>
2019-10-17 18:46:01 +0100

    Added back arrow to breadcrums

--- src/components/breadcrums/index.tsx
+++ src/components/breadcrums/index.tsx
@@ -1,10 +1,13 @@
 import React, { FC } from 'react'
 import { Link } from 'react-router-dom'
+import SVG from 'react-inlinesvg'
 import { Crums } from '~@components/breadcrums/styles'
+import back from '~@assets/icons/left-arrow.svg'
 
 const Breadcrums: FC = () => (
 	<Crums className="content">
 		<Link to="/blog">
+			<SVG src={back} alt="Back" width={100} />
 			<p>Blog</p>
 		</Link>
 	</Crums>
