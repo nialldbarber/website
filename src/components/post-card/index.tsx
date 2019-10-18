5f61056173e764b29f848c23a4a6a2b6527b7fee
Niall Barber <niall.barber@actual-experience.com>
2019-10-18 12:36:59 +0100

    Added types to post card

--- src/components/post-card/index.tsx
+++ src/components/post-card/index.tsx
@@ -1,10 +1,14 @@
 import React, { FC } from 'react'
-import PropTypes from 'prop-types'
 import SVG from 'react-inlinesvg'
 import { getIcon } from '~@utils/icons'
 import { Card } from '~@styles/styled-components/content'
 
-const PostCard: FC = ({ title, type }) => (
+interface Props {
+	title: string,
+	type: string
+}
+
+const PostCard: FC<Props> = ({ title, type }) => (
 	<Card>
 		<SVG src={getIcon(type)} alt="Icon" width={50} />
 		<h3>{title}</h3>
@@ -12,8 +16,3 @@ const PostCard: FC = ({ title, type }) => (
 )
 
 export default PostCard
-
-PostCard.propTypes = {
-	title: PropTypes.string,
-	type: PropTypes.string
-}
