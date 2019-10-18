1446aa0bf574fd698a032b2aed1ba062411ee446
Niall Barber <niall.barber@actual-experience.com>
2019-10-18 13:57:54 +0100

    Moved postcard & social types to separate file

--- src/components/post-card/index.tsx
+++ src/components/post-card/index.tsx
@@ -2,11 +2,7 @@ import React, { FC } from 'react'
 import SVG from 'react-inlinesvg'
 import { getIcon } from '~@utils/icons'
 import { Card } from '~@styles/styled-components/content'
-
-interface Props {
-	title: string,
-	type: string
-}
+import { Props } from '~@components/post-card/types'
 
 const PostCard: FC<Props> = ({ title, type }) => (
 	<Card>
